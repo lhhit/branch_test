4ada2cbe8e46da66065d932567f8914a3dd12c60
lhhit <lh_hit_1992@163.com>
2014-10-14 20:55:26 +0800

    Create ball.h

---
+++ ball.h
@@ -0,0 +1,30 @@
+#ifndef BALL_H
+#define BALL_H
+
+
+struct Bound
+{
+	int top ;
+	int left ;
+	int right ;
+	int bottom ;
+} ;
+// to insert some comments
+class Ball
+{
+private:
+	int x ; //pos
+	int y ;
+	int mx ; // addtion
+	int my ;
+	struct Bound *bound ;
+public :
+	//we add different comments
+	void init(int x, int y ,int mx , int my , struct Bound *bound) ;
+	bool move(int x1 , int x2) ;
+	bool isOut(int x1 , int x2) ;
+	void drawBall(CDC * pDC) ;
+
+} ;
+
+#endif
